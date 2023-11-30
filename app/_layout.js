808135d9e022daa96ef1fac8b4a0d2f2d1ed58c2
fason250 <jeyfason58@gmail.com>
2023-11-30 11:52:12 +0200

    first commit

---
+++ app/_layout.js
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router'
+
+
+const Layout= ()=>{
+    return(
+        
+        < Stack /> 
+    )
+}
+
+export default Layout
---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { View , Text } from 'react-native'
+
+const Home = ()=>{
+    return(
+        <View>
+            <Text> Home Page </Text>
+        </View>
+    )
+} 
